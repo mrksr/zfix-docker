7cfa88e3938ca0734e798fb961159f244f187054
Markus Kaiser <markus.kaiser@in.tum.de>
2017-01-06 01:51:15 +0100

    use proper certificate

--- applications/prosody/prosody.cfg.lua
+++ applications/prosody/prosody.cfg.lua
@@ -88,12 +88,8 @@ bosh_max_inactivity = 30;
 
 -- Encryption
 ssl = {
-  -- key = "/srv/CA/private/jabber.key.pem";
-  -- certificate = "/srv/CA/certs/jabber.crt.pem";
-  -- key = "/etc/letsencrypt/live/zfix.org/privkey.pem";
-  -- certificate = "/etc/letsencrypt/live/zfix.org/fullchain.pem";
-  key = "/etc/prosody/certs/localhost.key";
-  certificate = "/etc/prosody/certs/localhost.crt";
+  key = "/letsencrypt/privkey.pem";
+  certificate = "/letsencrypt/fullchain.pem";
 
   protocol = "tlsv1+";
   ciphers = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA";
