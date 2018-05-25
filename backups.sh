f7968500df012ddf952cffda6283495498b9e2c0
Markus Kaiser <markus.kaiser@in.tum.de>
2018-05-25 22:06:05 +0200

    Use a single backup script

--- backups.sh
+++ backups.sh
@@ -20,3 +20,16 @@ for container_id in $( docker ps --filter ancestor=mariadb --format {{.ID}} ); d
         --flush-logs --flush-privileges
         " | xz > $BACKUP_FOLDER/mariadb_${container_name}_latest.sql.xz
 done
+
+# Docker volumes
+for volume_id in $( docker volume ls -q ); do
+    # We do not backup 'unnamed' docker volumes
+    if [[ ! $volume_id =~ [0-9a-fA-F]{60} ]]; then
+        docker run \
+            -v ${volume_id}:/volume \
+            -v ${BACKUP_FOLDER}:/backup \
+            --rm \
+            loomchild/volume-backup \
+            backup volume_$volume_id
+    fi
+done
