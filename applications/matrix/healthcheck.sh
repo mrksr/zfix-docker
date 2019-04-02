7dc30f2beb5f8896b448e1cba3bd0f775a838d8a
Markus Kaiser <markus.kaiser@in.tum.de>
2019-04-02 15:18:26 +0200

    Fix healthcheck some more

--- applications/matrix/healthcheck.sh
+++ applications/matrix/healthcheck.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-if (find /data/logs | sort -r | head -n 1 | xargs tail -n 10 | grep -i 'error\|fatal' | grep -q -v 'download\|parse'); then
+if ( \
+    find /data/logs \
+    | sort -t'-' -n -k4 \
+    | sort -r -t'-' -s -k1,3 \
+    | head -n 1 \
+    | xargs tail -n 10 \
+    | grep -i 'error\|fatal' \
+    | grep -q -i -v 'download\|parse\|invalid\|handling' \
+    ); then
     pkill mautrix-whatsapp
     exit 1
 fi
