68c6e9eda4be2bc56bef62ff017b9ae06c2aa408
Markus Kaiser <markus.kaiser@in.tum.de>
2019-06-27 12:57:24 +0200

    Reenable 'healthcheck' to restart the bridge every few hours

--- applications/matrix/healthcheck.sh
+++ applications/matrix/healthcheck.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-if ( \
-    find /data/logs \
-    | sort -t'-' -n -k4 \
-    | sort -r -t'-' -s -k1,3 \
-    | head -n 1 \
-    | xargs tail -n 10 \
-    | grep -i 'error\|fatal' \
-    | grep -q -i -v 'download\|parse\|invalid\|handling' \
-    ); then
-    pkill mautrix-whatsapp
-    exit 1
-fi
+# if ( \
+#     find /data/logs \
+#     | sort -t'-' -n -k4 \
+#     | sort -r -t'-' -s -k1,3 \
+#     | head -n 1 \
+#     | xargs tail -n 10 \
+#     | grep -i 'error\|fatal' \
+#     | grep -q -i -v 'download\|parse\|invalid\|handling' \
+#     ); then
+#     pkill mautrix-whatsapp
+#     exit 1
+# fi
+
+pkill mautrix-whatsapp
+exit 1
